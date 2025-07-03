a8d72ec710dfee8eb1a2f42a4c30b994571145bb
StellateZero <stellatezero@outlook.com>
2025-07-03 20:22:50 +0800

    feat(sitemap): 添加 next-sitemap 配置文件以生成站点地图和 robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://live.s0tools.cn', // 你的站点网址，必须写正确
+  generateRobotsTxt: true, // 自动生成robots.txt
+  // 你还可以添加额外的配置，比如排除某些路径
+  // exclude: ['/secret-page', '/admin/*'],
+};
